abb8d9e3a1d58c2a28fee7dbddcc4889db296c53
stzheng7 <69952035+stzheng7@users.noreply.github.com>
2022-04-30 12:37:48 -0400

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // Require Express.js
+const http = require("http");
 const express = require('express')
 const app = express()
 
-const http = require("http");
 const args = require('minimist')(process.argv.slice(2));
 args['HTTP_PORT'];
 const HTTP_PORT = args.HTTP_PORT || process.env.HTTP_PORT || 5000;
@@ -49,26 +49,6 @@ function coinFlip() {
       return "tails";
     }
   }
-  
-  /** Multiple coin flips
-   * 
-   * Write a function that accepts one parameter (number of flips) and returns an array of 
-   * resulting "heads" or "tails".
-   * 
-   * @param {number} flips 
-   * @returns {string[]} results
-   * 
-   * example: coinFlips(10)
-   * returns:
-   *  [
-        'heads', 'heads',
-        'heads', 'tails',
-        'heads', 'tails',
-        'tails', 'heads',
-        'tails', 'heads'
-      ]
-   */
-  
   function coinFlips(flips) {
     const flipsA = [];
     for (var i = 0; i < flips; i++) {
@@ -76,19 +56,6 @@ function coinFlip() {
     }
     return flipsA;
   }
-  
-  /** Count multiple flips
-   * 
-   * Write a function that accepts an array consisting of "heads" or "tails" 
-   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
-   * an object containing the number of each.
-   * 
-   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
-   * { tails: 5, heads: 5 }
-   * 
-   * @param {string[]} array 
-   * @returns {{ heads: number, tails: number }}
-   */
   
   function countFlips(array) {
     const countA = {heads: 0, tails: 0};
@@ -102,18 +69,7 @@ function coinFlip() {
     }
     return countA;
   }
-  
-  /** Flip a coin!
-   * 
-   * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
-   * 
-   * @param {string} call 
-   * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
-   * 
-   * example: flipACoin('tails')
-   * returns: { call: 'tails', flip: 'heads', result: 'lose' }
-   */
-  
+
   function flipACoin(call) {
     const FlipA = {call: call, flip: "", result: ""}
     FlipA.flip = coinFlip();
